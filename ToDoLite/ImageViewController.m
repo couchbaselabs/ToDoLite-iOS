58d61de945c9a94712c9a64eff763a217b26f41d
Pasin Suriyentrakorn <pasin@couchbase.com>
2016-06-08 17:40:42 -0700

    Not using split view controller for iPad
    Simplify UI code by not using UISplitViewController for iPad.

--- ToDoLite/ImageViewController.m
+++ ToDoLite/ImageViewController.m
@@ -16,15 +16,13 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        self.wantsFullScreenLayout = YES;
-    }
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)]];
+    [self.imageView addGestureRecognizer:
+        [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)]];
     [self.imageView setImage:self.image];
 }
 
